d5ce8d374e33f700c7fd230cc12f7c9aed31f23e
beast4coder <matthewgwyndavies@gmail.com>
2023-08-03 16:25:53 +0100

    wasm intensifies

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,14 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("sprites/LLLA.png"),
+        ..default()
+    });
 }
