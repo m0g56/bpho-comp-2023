120038f9e32bbf787a1a79291d9b21e18a0441ea
m0g56 <morganparry12@outlook.com>
2023-08-13 16:01:33 +0100

    Task 1 complete

--- c1/c1.py
+++ c1/c1.py
@@ -1,33 +1,36 @@
 import mpld3 as mpld
 import matplotlib.pyplot as plt
-import math
 
-G = 6.67 * (10 ** -11)
-PI = math.pi
-M = 1.9891 * (10 ** 30)
-multiplier = (G * M) / (4 * (PI ** 2))
-
-def GenerateGraph():
-    fig, grph = plt.subplots()    
+def GenerateGraphs():
+    fig1, grph1 = plt.subplots() 
+    fig2, grph2 = plt.subplots()    
     xvals = []
-    OrbPeriod = [29.63, 84.75, 11.86, 166.34, 1.88, 0.62, 0.24, 1.00]
+    OrbPeriod = [29.63, 84.75, 11.86, 166.34, 248.35, 1.88, 0.62, 0.24, 1.00]
+    OrbPeriod.sort()
     for i in range(len(OrbPeriod)):
-        xvals.append(multiplier * OrbPeriod[i])
-    grph.plot(xvals, OrbPeriod, 'ro') 
-    plt.show()
-    #return mpld.fig_to_html(fig)
+        xvals.append(OrbPeriod[i]**(2/3))
+    grph1.xaxis.set_label_text('Orbital Radius / AU')
+    grph1.yaxis.set_label_text('Orbital Period / Years')
+    grph1.set_title('Kepler III Correlation')
+    grph1.plot(xvals, OrbPeriod, 'cD-')
+    grph2.xaxis.set_label_text('Orbital Radius / AU^3/2')
+    grph2.yaxis.set_label_text('Orbital Period / Years')
+    grph2.plot(OrbPeriod, OrbPeriod, 'rD-')  
+    plt.show()  
+    return [fig1, fig2]
 
 
-
-
-def GenerateScript(script):
-    script = "<!-- here -->\n\n" + script
-    with open('index.html', 'r') as f:
+def GenerateScript(graphs):
+    script = ""
+    for i in graphs:
+        script = script + mpld.fig_to_html(i) + "\n"
+    script = "<!-- here -->\n\n" + script 
+    with open('c1\c1.html', 'r') as f:
         lines = f.readlines()
     for i in range(len(lines)):
         if lines[i].__contains__("<!-- here -->"):
             lines[i] = script
-    with open('index.html', 'w') as f:
+    with open('c1\c1.html', 'w') as f:
         f.writelines(lines)
 
-GenerateGraph()
+yes = GenerateGraphs()
